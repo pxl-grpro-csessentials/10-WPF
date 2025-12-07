51a0f919c02a09f080c1c070981c2118e24e9088
Koen Bloemen <koen.bloemen@pxl.be>
2025-12-07 10:59:24 +0100

    EventPlanner

---
+++ EventPlanner/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EventPlanner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ EventPlanner/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventPlanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] _eventTypes = { "Festival", "Orkest", "Opera" };
+        struct _eventObject
+        {
+            public string eventType;
+            public string name;
+            public int visitors;
+        }
+        readonly List<_eventObject> _events = new List<_eventObject>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillTypes();
+        }
+
+        private void FillTypes()
+        {
+            foreach (string eventType in _eventTypes)
+            {
+                eventTypeComboBox.Items.Add(eventType);
+            }
+        }
+
+        private void delete_MenuItem(object sender, RoutedEventArgs e)
+        {
+            itemsListBox.Items.Clear();
+        }
+
+        private void default_MenuItem(object sender, RoutedEventArgs e)
+        {
+            AddEvent("Orkest", "Jaarlijks optreden", 100);
+        }
+
+        private void close_MenuItem(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (itemsListBox.SelectedIndex > -1)
+            {
+                _events.RemoveAt(itemsListBox.SelectedIndex);
+                itemsListBox.Items.RemoveAt(itemsListBox.SelectedIndex);
+            }
+
+            FillList();
+        }
+
+        private void AddEvent(string eventType, string naam, int aantalBezoekers)
+        {
+            _eventObject newEvent = new _eventObject();
+            newEvent.eventType = eventType;
+            newEvent.name = naam;
+            newEvent.visitors = aantalBezoekers;
+            _events.Add(newEvent);
+
+            FillList();
+        }
+
+        private void FillList()
+        {
+            itemsListBox.Items.Clear();
+            foreach (_eventObject evObject in _events)
+            {
+                itemsListBox.Items.Add($"{evObject.eventType} - {evObject.name}: {evObject.visitors}");
+            }
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddEvent(eventTypeComboBox.SelectedValue.ToString(), nameEventTextBox.Text, int.Parse(numberOfVisitorsTextBox.Text));
+        }
+    }
+}
