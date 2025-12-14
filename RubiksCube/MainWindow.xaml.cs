458a4c46fae5ff21c47de4f970b3ce066c78d50d
Wim Bervoets <wim.bervoets@pxl.be>
2025-12-14 23:36:00 +0100

    added rubikscube

---
+++ RubiksCube/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace RubiksCube
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Brush[] _cubeColors = { Brushes.Blue, Brushes.Green, Brushes.Yellow, Brushes.Red, Brushes.White, Brushes.Orange };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            foreach (Label childLabel in rubriksGrid.Children)
+            {
+                Brush background = childLabel.Background;
+
+                int index = Array.IndexOf(_cubeColors, background) + 1;
+
+                if (index == _cubeColors.Length)
+                    index = 0;
+
+                childLabel.Background = _cubeColors[index];
+            }
+        }
+    }
+}
